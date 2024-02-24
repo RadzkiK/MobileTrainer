809b9b78e2505b7c0143047138c775206ea5f3a7
RadzkiK <k.domeradzki@outlook.com>
2024-02-24 19:08:31 +0100

    Add home screen and animations in navigation

--- app/src/main/java/com/engineer/mobiletrainer/Home.kt
+++ app/src/main/java/com/engineer/mobiletrainer/Home.kt
@@ -1,45 +1,78 @@
 package com.engineer.mobiletrainer
 
-import android.content.Intent
+import android.graphics.ImageDecoder
+import android.graphics.drawable.AnimatedImageDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.Button
+import android.widget.ImageView
+import androidx.navigation.findNavController
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
+
+
 /**
  * A simple [Fragment] subclass.
- * Use the [Training.newInstance] factory method to
+ * Use the [Home.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Training : Fragment() {
+class Home : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-
     }
 
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        startActivity(Intent(context, TrainingActivity::class.java))
-        return inflater.inflate(R.layout.fragment_training, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val listener = ImageDecoder.OnHeaderDecodedListener { decoder,_,_ ->
+            decoder.setOnPartialImageListener { decodeException ->
+                true
+            }
+        }
+        val imageView = view.findViewById<ImageView>(R.id.imageView)
+
+        GlobalScope.launch(Dispatchers.Default) {
+            // worker thread
+            val source = ImageDecoder.createSource(requireContext().resources, R.drawable.home_background)
+            val drawable = ImageDecoder.decodeDrawable(source,listener)
+            GlobalScope.launch(Dispatchers.Main) {
+                imageView.setImageDrawable(drawable)
+                if(drawable is AnimatedImageDrawable) {
+                    drawable.start()
+                }
+            }
+        }
+
+        view.findViewById<Button>(R.id.home_button).setOnClickListener(View.OnClickListener {
+                view -> view.findNavController().navigate(R.id.action_home2_to_exerciseChoice)
+        })
+
+
+        return view
     }
+
     companion object {
         /**
          * Use this factory method to create a new instance of
@@ -47,19 +80,16 @@ class Training : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Training.
+         * @return A new instance of fragment Home.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Training().apply {
+            Home().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
     }
-
-
-
 }
